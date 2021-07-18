98815898f36ea091e8584c1690ecb9a3a9606980
luis cortazar <lcortazar4@gmail.com>
2021-07-18 07:21:11 -0600

    Added list,images,and onclick alerts

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 const h2 = document.createElement('h2')
 
-h2.textContent = "Nalichenta";
+h2.textContent = "Who is Nalichenta? ";
 
 document.querySelector('body').appendChild(h2);
 
+const ball=document.createElement('p');
+
+ball.textContent = "Throw it Already!!!";
+
+
+function alertMe(element) {
+    alert(ball.innerText)
+}
+ const friend=document.createElement('p');
+
+ friend.textContent = "Hello! I am Nalichenta's best friend"
+
+ function alertMe(element) {
+    alert(friend.innerText)
+}
